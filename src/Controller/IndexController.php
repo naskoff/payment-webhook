0c5023e110ea8e27f72471bbf26e9c928d8587bc
Zlatin Hristov <zlatin.hristov@giftcards.eu>
2021-09-28 13:33:50 +0300

    Added log message for every requests

---
+++ src/Controller/IndexController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class IndexController extends AbstractController
+{
+    /**
+     * @Route("/", name="webhook_handler")
+     */
+    public function index(Request $request): Response
+    {
+        return new Response('Ok');
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/IndexController.php',
+        ]);
+    }
+}
